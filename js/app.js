eed3f75422f7969bc0d44bcb36f66bff37019b8e
Victor de Assis <victoralvespda@gmail.com>
2016-07-13 08:26:30 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,62 @@
+var myApp = angular.module('myApp', ['firebase']);
+
+myApp.controller('ProductsCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
+
+  var myProducts = new Firebase('https://radiant-heat-6955.firebaseio.com/products');
+
+  $scope.products = $firebaseArray(myProducts);
+
+  $scope.showForm = function() {
+    $scope.addFormShow = true;
+    $scope.editFormShow = false;
+    clearForm();
+  }
+
+  $scope.hideForm = function() {
+    $scope.addFormShow = false;
+  }
+
+  function clearForm() {
+    $scope.productName = '';
+    $scope.productCode = '';
+    $scope.description = '';
+    $scope.price = '';
+  }
+
+  $scope.addFormSubmit = function() {
+    $scope.products.$add({
+      productName:$scope.productName,
+      productCode:$scope.productCode,
+      description:$scope.description,
+      price:$scope.price
+    });
+
+    clearForm();
+  }
+
+  $scope.showProduct = function(product) {
+    $scope.editFormShow = true;
+    $scope.addFormShow = false;
+    $scope.productName = product.productName;
+    $scope.productCode = product.productCode;
+    $scope.description = product.description;
+    $scope.price = product.price;
+    $scope.id=product.$id;
+  }
+
+  $scope.editFormSubmit = function() {
+    var id = $scope.id;
+    var record = $scope.products.$getRecord(id);
+    record.productName = $scope.productName;
+    record.productCode = $scope.productCode;
+    record.description = $scope.description;
+    record.price = $scope.price;
+
+    $scope.products.$save(record);
+  }
+
+  $scope.deleteProduct = function(product) {
+    $scope.products.$remove(product);
+  }
+
+}]);
